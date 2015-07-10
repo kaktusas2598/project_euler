f9834596de191b193045ec0cbf799a2a88412e87
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2015-07-10 23:40:06 +0000

    Added C++ solution to problem 10

---
+++ 10/summation_of_primes.cpp
@@ -0,0 +1,31 @@
+// Find sum of all primes below two million
+#include <iostream>
+#include <math.h> //Compile with -lm
+
+using namespace std;
+
+int main(){
+
+	long long sum = 0;
+	for(int i = 2; i < 2000000; i++){
+		bool primeTest = true;
+		//2 and 3 is primes
+		if(i == 2 || i == 3)
+			sum += i;
+		//Test prime
+		else{
+			for(int j=2;j<sqrt((float)i);j++){
+				//If number n between 1 and sqrt(n) have any
+				//multiples it is prime
+				if(i % j == 0){
+					primeTest = false;
+					break;
+				}
+			}
+			if(primeTest)
+				sum += i;
+		}
+	}
+	cout << "Sum of primes bellow two million: " << sum << endl;
+	return 0;
+}
