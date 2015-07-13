a68e5f2c131c8832185ca5108f0451fa6160bd21
kaktusas2598 <nerijus.vilcinskas@gmail.com>
2015-07-13 11:55:31 +0000

    Fixed 13th problem file input and did a little work to string manipulation

--- 13/large_sum.cpp
+++ 13/large_sum.cpp
@@ -8,7 +8,7 @@
 
 using namespace std;
 
-string large_sum(string a,string b);
+string large_sum(string &a,string &b);
 int main(){
 
 	int countNum = 100;
@@ -20,9 +20,7 @@ int main(){
 	//read numbers
 	string  num [countNum];
 	for(int i=0; i< countNum;i++){
-		//TODO: FIXME
 		getline(numbers, num[i]);
-		cout << num[i] << endl;
 	}
 
 	//sum all numbers
@@ -33,9 +31,10 @@ int main(){
 
 	return 0;
 }
-string large_sum(string a,string b){
-	string sum,max,min;
+string large_sum(string &a,string &b){
+	string res,max,min;
 	int carry = 0;
+	int sum = 0;
 
 	if(a.length() > b.length()){
 		max = a;
@@ -49,12 +48,12 @@ string large_sum(string a,string b){
 	//add min to max
 	cout << max << endl;
 	int pos_min = min.length() - 1;
-	for(int pos_max = max.length() - 1;pos_max > 0;pos_max++){
-		int sum = (int)max[pos_max] - 48;
-		cout << max[pos_max] << endl;
+	for(int pos_max = max.length() - 1;pos_max >= 0;pos_max--){
+		sum = max[pos_max] - '0';
+		cout << sum << endl;
 	}
 
 
 
-	return sum;
+	return res;
 }
